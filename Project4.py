c91362a71dbcc46bbd8e195e9febabaa95177ce1
khoint5304 <sherlockholmes5304@gmail.com>
2024-03-18 10:23:13 +0700

    Fixed Project4.py

--- Project4.py
+++ Project4.py
@@ -1,31 +1,47 @@
 from docplex.mp.model import Model
 
 
-def solve_optimal_transport(N, M, Q, d, q):
+# Example data
+N = 3  # Number of product types
+M = 4  # Number of shelves
+# Quantity of products on each shelf
+Q = [[0, 1, 2, 3, 0], [0, 2, 1, 0, 3], [0, 0, 3, 2, 1]]
+d = [[0, 1, 2, 3, 4], [1, 0, 1, 2, 3], [2, 1, 0, 1, 2],
+     [3, 2, 1, 0, 1], [4, 3, 2, 1, 0]]  # Distance between shelves
+q = [2, 2, 1]  # Quantity of each product to pick
+
+
+def solve_optimal_transport():
     model = Model('optimal_transport')
 
     # Biến
-    x = {(i, j): model.binary_var(name='x_{0}_{1}'. format(i, j))
-         for i in range(M) for j in range(M)}
+    x = {(i, j): model.binary_var(name='x_{0}_{1}'. format(i, j)) for i in range(M + 1) for j in range(M + 1)}
 
     # Giải
-    model.minimize(model.sum(d[i][j] * x[i, j]
-                   for i in range(M+1) for j in range(M+1)))
+    model.minimize(model.sum(d[i][j] * x[i, j] for i in range(M + 1) for j in range(M + 1)))
 
     # Chuyến đi luôn đi từ bến và kết thúc ở bến
-    model.add_constraint(model.sum_vars(x[0, j] for j in range(M + 1)) == 1)
-    model.add_constraint(model.sum_vars(x[i, 0] for i in range(M + 1)) == 1)
+    model.add_constraint(model.sum_vars(x[0, j] for j in range(1, M + 1)) == 1)
+    model.add_constraint(model.sum_vars(x[i, 0] for i in range(1, M + 1)) == 1)
 
     # không có chuyến đi nào xuất phát và kết thúc ở cùng 1 điểm
-    model.add_constraint(model.sum_vars(x[i, i] for i in range(M)) == 0)
+    model.add_constraint(model.sum_vars(x[i, i] for i in range(M + 1)) == 0)
 
-    # mỗi điểm đi qua 1 lần
-    model.add_constraint(model.sum_vars(
-        x[i, j] + x[j, k] for i in range(M + 1)for j in range(M + 1)for k in range(M + 1)) == 2)
+    for i in range(M + 1):
+        model.add_constraint(model.sum(x[i, j] for j in range(M + 1)) == 1)
+    
+    for j in range(M + 1):
+        model.add_constraint(model.sum(x[i, j] for i in range(M + 1)) == 1)
+    
+
+    #không xoay vòng n điểm
+    for i in range(M + 1):
+        for j in range(M + 1):
+            model.add_constraint((x[j, i] + x[i, j] ) <= 1)    # type: ignore
 
     # tổng số lượng hàng lấy ở các điểm chứa hàng sẽ lớn hơn số hàng yêu cầu lấy
-    model.add_constraint(model.sum_vars(
-        Q[j] * x[i, j] for i in range(M) for j in range(M)) <= q[k] for k in range(N))
+    for k in range(N):
+        model.add_constraint(model.sum(Q[k][j] * x[i, j] for i in range(M + 1) for j in range(M + 1)) >= q[k])  # type: ignore
 
     # giải kết quả
     model.solve()
@@ -33,14 +49,11 @@ def solve_optimal_transport(N, M, Q, d, q):
     if model.solution is not None:
         print("Total distance:", model.objective_value)
 
+    for i in range(M + 1):
+        for j in range(M + 1):
+            print(int(x[i, j].solution_value), end=" ")
+        
+        print()
 
-# Example data
-N = 3  # Number of product types
-M = 4  # Number of shelves
-# Quantity of products on each shelf
-Q = [[1, 2, 3, 0], [2, 1, 0, 3], [0, 3, 2, 1]]
-d = [[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 1],
-     [3, 2, 1, 0]]  # Distance between shelves
-q = [2, 2, 1]  # Quantity of each product to pick
 
-solve_optimal_transport(N, M, Q, d, q)
+solve_optimal_transport()
